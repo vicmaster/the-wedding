419664ea2f91a98d3dc0091fac4293b054a922e4
Victor Velazquez <velazquezgaspar16@hotmail.com>
2016-11-15 03:03:20 -0600

    Add routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 D::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root 'users#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  resources :invitations
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
