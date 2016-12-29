73356bfc05881836105e2eb2572322e4703499d4
Victor Velazquez <velazquezgaspar16@hotmail.com>
2016-12-29 04:36:25 -0600

    Fix accept invitation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def index ; end
+  def index
+    @invite = Invite.new(invite_params)
+    @invite.user_id = current_user.id if current_user
+  end
+
+  private
+
+  def invite_params
+    params[:invite]
+  end
 end
